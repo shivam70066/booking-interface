86a4f238712c46ddf098db699b0295c01e119ae3
Garry <garry@lodgingsystem.com>
2024-07-29 17:34:43 +0530

    overlay module implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet,RouterLink } from '@angular/router';
+import { Component , ViewEncapsulation} from '@angular/core';
+import { RouterOutlet,RouterLink, Router } from '@angular/router';
+import {OverlayModule} from '@angular/cdk/overlay';
+import { DisplayPropertiesComponent } from "./components/display-properties/display-properties.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink, OverlayModule, DisplayPropertiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'booking-interface';
   showBookButton : boolean = true;
+  isOpen = false;
 
-  constructor(){
+  constructor(private router: Router){
 
-  }
-
-  hello(){
-    console.log('hello');
   }
 
   toggleBookingInterface(){
     this.showBookButton = !this.showBookButton;
-
+    if (!this.showBookButton) {
+      this.router.navigate(['/properties']);
+      return;
+    }
+    this.router.navigate(['']);
   }
 }
+
